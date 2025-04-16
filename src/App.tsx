a62f68fee38d3cd279e71ee339b84085f411fa57
Ravi Kumar Patel <Ravi.Patel@almonds.ai>
2025-04-16 20:30:21 +0530

    website opened

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
-import Maintenance from "./pages/Maintenance";
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import About from "./pages/About";
-// other imports...
+import Contact from "./pages/Contact";
+import Blog from "./pages/Blog";
+import BlogPost from "./pages/BlogPost";
+import Maintenance from "./pages/Maintenance";
 
-const App = () => {
-  const showMaintenance = true; // <<< Set to false when you're ready to go live
 
-  return (
-    <BrowserRouter>
-      {showMaintenance ? (
-        <Maintenance />
-      ) : (
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
         <Routes>
+          {/* Default routes */}
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
-          {/* Add your other routes here */}
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:slug" element={<BlogPost />} />
+
+          {/* Language-specific routes */}
+          <Route path="/:lang" element={<Index />} />
+          <Route path="/:lang/about" element={<About />} />
+          <Route path="/:lang/contact" element={<Contact />} />
+          <Route path="/:lang/blog" element={<Blog />} />
+          <Route path="/:lang/blog/:slug" element={<BlogPost />} />
         </Routes>
-      )}
-    </BrowserRouter>
-  );
-};
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
 
 export default App;
