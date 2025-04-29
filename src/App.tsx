de4dd77917e1e53351a45a840da81cf933b9f3c1
Ravi Kumar Patel <Ravi.Patel@almonds.ai>
2025-04-29 19:50:35 +0530

    toaster posistion fix

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
-      <Sonner />
+      <Sonner position="top-center" richColors />
       <BrowserRouter>
         <Routes>
           {/* Default routes */}
